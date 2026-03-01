3bc4510601599b2154ec879974ecd76e6ffc9bd4
shun144 <noelgld13@gmail.com>
2026-03-02 07:42:04 +0900

    first commit

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    expect(true).toBe(true);
+  });
+});
